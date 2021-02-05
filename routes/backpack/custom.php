bf6e7e76dbb48d08801d3ae3c59f7fbd5b825dcb
hcijr6 <57660681+hcijr6@users.noreply.github.com>
2021-02-05 16:35:55 +0100

    order_line

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -5,6 +5,7 @@
 // --------------------------
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
+use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
@@ -18,4 +19,5 @@ Route::group([
     Route::crud('product', 'ProductCrudController');
     Route::crud('provider', 'ProviderCrudController');
     Route::crud('user', 'UserCrudController');
+    Route::crud('order_line', 'Order_lineCrudController');
 }); // this should be the absolute last line of this file
